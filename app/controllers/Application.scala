7a0c1fd5b5283c053d9f44ed940fa064b784865b
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2015-04-25 19:18:34 +0900

    Add to section 1.3.8

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,25 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
 
+  val form = Form[String](
+    "message" -> text
+  )
+
   def index = Action {
-    Ok(views.html.index.render("This is first : ", "This is second : ", "This is last : "))
+    Ok(views.html.index.render("Write some words: ", form))
   }
 
+  def send = Action(implicit request =>
+    form.bindFromRequest().fold(
+      errors => BadRequest(views.html.index.render("Error: ", form)),
+      success = text => {
+        Ok(views.html.index.render("Write some words: " + text, form))
+      }
+    )
+  )
 }

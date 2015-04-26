a55f74ffa8e5de2183ae7553a114e0f1105b7f66
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2015-04-26 23:13:07 +0900

    Add DAO

--- app/models/Message.scala
+++ app/models/Message.scala
@@ -3,9 +3,6 @@ package models
 import java.sql.Timestamp
 import play.api.db.slick.Config.driver.simple._
 
-/**
- * Created by yad on 15/04/25.
- */
 case class Message(id: Long, name: String, mail: String, message: String, postDate: Timestamp)
 
 class MessageTable(tag: Tag) extends Table[Message](tag, "messages") {
@@ -15,4 +12,28 @@ class MessageTable(tag: Tag) extends Table[Message](tag, "messages") {
   def message = column[String]("message", O.NotNull)
   def postDate = column[Timestamp]("postDate", O.NotNull)
   def * = (id, name, mail, message, postDate) <> (Message.tupled, Message.unapply)
+}
+
+object MessageDAO {
+  lazy val messageQuery = TableQuery[MessageTable]
+
+  def read(id: Long)(implicit session: Session): Message = {
+    messageQuery.filter(_.id === id).first
+  }
+
+  def readAll(implicit session: Session): List[Message] = {
+    messageQuery.list
+  }
+
+  def create(message: Message)(implicit session: Session): Unit = {
+    messageQuery.insert(message)
+  }
+
+  def update(message: Message)(implicit session: Session): Unit = {
+    messageQuery.filter(_.id === message.id).update(message)
+  }
+
+  def delete(id: Long)(implicit session: Session) = {
+    messageQuery.filter(_.id === id).delete
+  }
 }
